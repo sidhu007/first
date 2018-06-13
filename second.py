ff02d43126e6618924e7784c46993fbd9367f22a
sidhu007 <skgouri0007@gmail.com>
2018-06-13 07:47:32 +0530

    My first repo

---
+++ second.py
@@ -0,0 +1,12 @@
+a=1
+b=3.32
+c=13+112j
+
+print('Type of a = ',type(a))
+print('Type of b = ',type(b))
+print('Type of c = ',type(c))
+
+print('Value of a = ',a)
+print('Value of b = ',b)
+print('Value of c = ',c)
+
